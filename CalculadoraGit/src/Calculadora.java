cf56117b50489dbe32a8797aa4d543217bd0eb1a
AdrianDW7 <adomwen0706@g.educaand.es>
2026-03-24 11:47:20 +0100

    Commit inicial

---
+++ CalculadoraGit/src/Calculadora.java
@@ -0,0 +1,18 @@
+/**
+ * Clase que representa una calculadora con operaciones aritméticas básicas.
+ *
+ * @author Adrián
+ */
+public class Calculadora {
+
+    /**
+     * Suma dos números enteros.
+     *
+     * @param a Primer sumando
+     * @param b Segundo sumando
+     * @return Resultado de la suma
+     */
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}
