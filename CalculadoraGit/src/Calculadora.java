105d41325f6d7b994291265477d11c6bd18ee0dc
AdrianDW7 <adomwen0706@g.educaand.es>
2026-03-24 17:31:46 +0100

    Corregida división por cero

--- CalculadoraGit/src/Calculadora.java
+++ CalculadoraGit/src/Calculadora.java
@@ -49,6 +49,9 @@ public class Calculadora {
      * @throws IllegalArgumentException Si el divisor es cero
      */
     public double dividir(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir entre cero");
+        }
         return (double) a / b;
     }
 }
